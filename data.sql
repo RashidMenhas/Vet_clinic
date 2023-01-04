5c380b28f4ba2455857ca58427c34e6668b3dc95
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-05 00:37:01 +0500

    create database and also table in a schema. use insert query in data.sql file and also make a query for the project requirement

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  values('Agumaon','02-03-2020',0,true,10.23), 
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)  values('Gabumon', '11-15-2018',2,true,8);
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg) values('Pikachu','01-07-2021',1,false,15.04);
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg) values('Devimon','05-12-2017',5,true,11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+ select * from animals where name like '%mon';
+ select name from animals where date_of_birth between '01-01-2016' and '01-01-2019';
+ select name from animals where neutered = true and escape_attempts <3;
+ select name , escape_attempts from animals where weight_kg > 10.5;
+ select * from animals where neutered = true;
+ select * from animals where name !='Gabumon';
+ select * from animals where weight_kg >=10.4 and weight_kg <=17.3;
+
