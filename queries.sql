347b2b24598a9a7b833550d0aecbcc3f1f670459
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-05 11:30:58 +0500

    Add the query in the queries.sql files and remove the comma from data.sql

--- queries.sql
+++ queries.sql
@@ -3,6 +3,7 @@
  select * from animals where name like '%mon';
  select name from animals where date_of_birth between '01-01-2016' and '01-01-2019';
  select name from animals where neutered = true and escape_attempts <3;
+ select date_of_birth from animals where name ='Agumon' or name ='Pikachu';
  select name , escape_attempts from animals where weight_kg > 10.5;
  select * from animals where neutered = true;
  select * from animals where name !='Gabumon';
